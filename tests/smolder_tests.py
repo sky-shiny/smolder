64b24fd898edde932c901ddbba867a3433dccfbf
mcameron <max.cameron@johngaltsystems.com>
2015-05-28 21:39:37 +0100

    Fixing tests for version 0.2

--- tests/smolder_tests.py
+++ tests/smolder_tests.py
@@ -4,33 +4,29 @@ import nose
 import json
 import os
 from nose.tools import assert_raises
-from imp import reload
 
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
 def test_noop_test():
-  assert smolder.noop_test()
+    assert smolder.noop_test()
 
 def test_github_status():
-  myfile = open(THIS_DIR + '/github_status.json')
-  test_json = json.load(myfile)
-  for test in test_json['tests']:
-    smolder.http_test(test, 'status.github.com', False)
-  reload(smolder)
-  assert smolder.failed_tests == 0
+    myfile = open(THIS_DIR + '/github_status.json')
+    test_json = json.load(myfile)
+    for test in test_json['tests']:
+        test_obj = smolder.charcoal.Charcoal(test=test, host='status.github.com')
+    assert test_obj.failed == 0
 
 def test_github_status_response_time_expect_fail():
-  myfile = open(THIS_DIR + '/harsh_github_status.json')
-  test_json = json.load(myfile)
-  for test in test_json['tests']:
-    test_obj = smolder.charcoal.Charcoal(test=test, host=kwargs['host'])
-    smolder.http_test(test, 'status.github.com', False)
-  reload(smolder)
-  assert smolder.failed_tests > 0
+    myfile = open(THIS_DIR + '/harsh_github_status.json')
+    test_json = json.load(myfile)
+    for test in test_json['tests']:
+        test_obj = smolder.charcoal.Charcoal(test=test, host='status.github.com')
+    assert test_obj.failed > 0
 
 def test_tcp_test():
-  smolder.tcp_test('127.0.0.1', 22) #Are you running an ssh server?
+    smolder.tcp_test('127.0.0.1', 22) #Are you running an ssh server?
 
 def test_fail_tcp_test():
-  assert_raises(Exception, smolder.tcp_test, '127.0.0.1', 4242)
+    assert_raises(Exception, smolder.tcp_test, '127.0.0.1', 4242)
 
