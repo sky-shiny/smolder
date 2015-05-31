6cb09a297347c9c4364deb035681655e98d58e10
Ali Asad Lotia <ali.asad.lotia@zeebox.com>
2015-05-31 02:22:39 +0100

    Add yaml tests. Move fixture data.

--- tests/smolder_tests.py
+++ tests/smolder_tests.py
@@ -5,14 +5,14 @@ import os
 from nose.tools import assert_raises
 from imp import reload
 
-THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fixtures')
 
 def test_noop_test():
   assert smolder.noop_test()
 
 
 def test_github_status():
-  myfile = open(THIS_DIR + '/github_status.json')
+  myfile = open(os.path.join(FIXTURE_DIR, 'github_status.json'), 'rb')
   test_json = yaml.load(myfile)
   for test in test_json['tests']:
     smolder.http_test(test, 'status.github.com', False)
@@ -20,13 +20,31 @@ def test_github_status():
   assert smolder.failed_tests == 0
 
 
+def test_github_status_yaml_format():
+    myfile = open(os.path.join(FIXTURE_DIR, 'github_status.yaml'), 'rb')
+    test_json = yaml.load(myfile)
+    for test in test_json['tests']:
+        smolder.http_test(test, 'status.github.com', False)
+    reload(smolder)
+    assert smolder.failed_tests == 0
+
+
 def test_github_status_response_time_expect_fail():
-  myfile = open(THIS_DIR + '/harsh_github_status.json')
+  myfile = open(os.path.join(FIXTURE_DIR, 'harsh_github_status.json'), 'rb')
   test_json = yaml.load(myfile)
   for test in test_json['tests']:
     smolder.http_test(test, 'status.github.com', False)
   reload(smolder)
   assert smolder.failed_tests > 0
+
+
+def test_github_status_response_time_expect_fail_yaml_format():
+    myfile = open(os.path.join(FIXTURE_DIR, 'harsh_github_status.yaml'), 'rb')
+    test_json = yaml.load(myfile)
+    for test in test_json['tests']:
+        smolder.http_test(test, 'status.github.com', False)
+    reload(smolder)
+    assert smolder.failed_tests > 0
 
 
 def test_tcp_test():
