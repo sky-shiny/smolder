ddb004b11331c80999cd228c343f26160e87156c
Max <maxwell.cameron@bskyb.com>
2015-07-06 13:48:09 +0100

    Fixing json validation plugin

--- smolder/validate_json.py
+++ smolder/validate_json.py
@@ -8,7 +8,7 @@ class ValidateJson(IPlugin):
     def run(req):
         try:
             response_json = req.req.json()
-            # validictory.validate(response_json, req.test['outcomes']['schema'])
-            # return req.pass_test("Json response matches the schema provided")
+            validictory.validate(response_json, req.test['outcomes']['validate_json']['schema'])
+            return req.pass_test("Json response matches the schema provided")
         except ValueError as error:
             return req.fail_test("Json response fails to match the schema provided {0}".format(error))
