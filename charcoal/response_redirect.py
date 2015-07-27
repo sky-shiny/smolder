782620f21216d7f3c9989557e89283e90caf475d
Max <maxwell.cameron@bskyb.com>
2015-07-27 11:37:24 +0100

    Catching cases where we are testing a redirect for a test which isn't redirected

--- charcoal/response_redirect.py
+++ charcoal/response_redirect.py
@@ -7,10 +7,13 @@ class ResponseRedirect(IPlugin):
 
     @staticmethod
     def run(req):
-        if req.test['outcomes']['response_redirect'] == req.req.headers['location']:
-            message = req.pass_test("Redirect to {0}".format(req.test['outcomes']['response_redirect']))
-            return message
+        if "30" in  str(req.req.status_code):
+            if req.test['outcomes']['response_redirect'] == req.req.headers['location']:
+                message = req.pass_test("Redirect to {0}".format(req.test['outcomes']['response_redirect']))
+                return message
+            else:
+                return req.fail_test("Got redirected to {0} instead of {1}".format(
+                    req.req.headers['location'],
+                    req.test['outcomes']['response_redirect']))
         else:
-            return req.fail_test("Got redirected to {0} instead of {1}".format(
-                req.req.headers['location'],
-                req.test['outcomes']['response_redirect']))
+            return req.fail_test("Not being redirected: {0}".format(req.req.status_code))
