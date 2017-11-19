385df80e83c8370bfe285948242f39e20e99db24
Max Cameron <max.cameron@johngaltsystems.com>
2017-11-19 22:25:22 +0000

    Adding LDAP support

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='smolder',
-    version='0.5.9.2',
+    version='0.6.0.0',
     description='Json wrapper around requests for simple smoke testing.',
     long_description=README,
     author='Max Cameron',
